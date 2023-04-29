2fd210ff9f0dcce1a1e2377352c8129a53a4b5ba
Asma-Ha <ja_hamidi@esi.dz>
2023-04-29 15:45:44 +0100

    Settings : Profile settings

--- Frontend/src/pages/settings.jsx
+++ Frontend/src/pages/settings.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import AnimatedBg from "../components/AnimatedBg";
 import SettingsNavBar from "../components/SettingsNavBar";
 import SideBar from "../components/SideBar"
+import SettingsContent from "../components/SettingsContent";
 import Footer from "../components/Footer";
 
 class Settings extends React.Component {
     render() {
         return (
-            <div className="bg-gradient-to-b from-page-light-dark to-page-dark w-full h-full">
+            <div className="bg-gradient-to-b from-page-light-dark to-page-dark relative">
             <AnimatedBg />
-            <div className="page-container absolute top-0 left-0 z-10 px-20 overflow-x-hidden overflow-y-auto">
+            <div className="relative z-10">
                 <SettingsNavBar />
-                <div className="flex mt-12 gap-16 mb-16">
+                <div className="flex mt-12 gap-16 mb-16 ml-16">
                     <SideBar />
-                    
+                    <SettingsContent /> 
                 </div>
                 <Footer />
             </div>
