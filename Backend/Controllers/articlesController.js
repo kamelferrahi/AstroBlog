b7ebd78011c9c68d84da9c88fdca34af024ad650
touahriayacine <medyacinetouahria@gmail.com>
2023-08-02 01:40:11 +0100

    adding write new article

--- Backend/Controllers/articlesController.js
+++ Backend/Controllers/articlesController.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
 }).promise();
 
 async function getAllArticles() {
-    const [rows] = await pool.query("SELECT R.id, title, description, date, time, img, comments, article_likes , article_dislikes, user_name, user_profile, user_likes, user_publications, c.community_name, c.profile_img as community_profile FROM(SELECT a.id, title, article_description as description, DATE_FORMAT(date_time, '%M %e, %Y') as date, DATE_FORMAT(date_time, '%H:%i') as time, article_img as img, nb_comments as comments, a.nb_likes as article_likes , nb_dislikes as article_dislikes, fullname as user_name, profile_pic as user_profile, u.nb_likes as user_likes, nb_publications as user_publications, community  FROM article a join user u on a.author = u.id) as R join community c on c.id = R.community");
+    const [rows] = await pool.query("SELECT R.id, title, description, date, time, img, comments, article_likes , article_dislikes, user_name, user_profile, user_likes, user_publications, c.community_name, c.profile_img as community_profile FROM(SELECT a.id, title, article_description as description, DATE_FORMAT(date_time, '%M %e, %Y') as date, DATE_FORMAT(date_time, '%H:%i') as time, article_img as img, nb_comments as comments, a.nb_likes as article_likes , nb_dislikes as article_dislikes, fullname as user_name, profile_pic as user_profile, u.nb_likes as user_likes, nb_publications as user_publications, community  FROM article a join user u on a.author = u.id) as R join community c on c.id = R.community order by date, time desc");
     let articles = [];
     for (let i = 0; i < rows.length; i++) {
         let row = rows[i];
@@ -28,7 +28,7 @@ async function getArticleFields(id) {
 }
 
 async function getArticleWithContent(id) {
-    const [row] = await pool.query("SELECT R.id, title, description, date, time, img, comments, article_likes , article_dislikes, content, user_name, user_profile, user_likes, user_publications, c.community_name, c.profile_img as community_profile FROM(SELECT a.id, title, article_description as description, DATE_FORMAT(date_time, '%M %e, %Y') as date, DATE_FORMAT(date_time, '%H:%i') as time, article_img as img, nb_comments as comments, a.nb_likes as article_likes , nb_dislikes as article_dislikes, content, fullname as user_name, profile_pic as user_profile, u.nb_likes as user_likes, nb_publications as user_publications, community  FROM article a join user u on a.author = u.id where a.id = ?) as R join community c on c.id = R.community", [id]);
+    const [row] = await pool.query("SELECT R.id, title, description, date, time, img, comments, article_likes , article_dislikes, content, user_name, user_profile, user_likes, user_publications, c.community_name, c.profile_img as community_profile FROM(SELECT a.id, title, article_description as description, DATE_FORMAT(date_time, '%M %e, %Y') as date, DATE_FORMAT(date_time, '%H:%i') as time, article_img as img, nb_comments as comments, a.nb_likes as article_likes , nb_dislikes as article_dislikes, content, fullname as user_name, profile_pic as user_profile, u.nb_likes as user_likes, nb_publications as user_publications, community  FROM article a join user u on a.author = u.id where a.id = ?) as R join community c on c.id = R.community order by date,time desc", [id]);
     if (row[0]) {
         let article = row[0];
         const fields = await getArticleFields(id);
@@ -44,4 +44,16 @@ async function getArticle(id) {
     return row;
 }
 
-module.exports = { getAllArticles, getArticle, getArticleWithContent };
+async function createArticle(article) {
+    const cdate = new Date(Date.now());
+    const fields = article.fields;
+    const [row1] = await pool.query("INSERT into article (author , community , date_time, title , article_description , article_img , content) values (? , ? ,? ,? ,? , ? , ?) ", [article.author, article.community, cdate, article.title, article.article_description, article.article_img, article.content]);
+    let row3 = undefined;
+    if (row1.affectedRows > 0 && fields && fields.length > 0) {
+        const combo = fields.map(f => `('${f}',${row1.insertId})`);
+        [row3] = await pool.query("INSERT into field values " + combo.toString(), []);
+    }
+    return row3 ? row3 : row1;
+}
+
+module.exports = { getAllArticles, getArticle, getArticleWithContent, createArticle };
--- Frontend/src/pages/writeArticle.jsx
+++ Frontend/src/pages/writeArticle.jsx
@@ -3,23 +3,32 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import feather from "../assets/icons/feather.png";
 import WriteArticleForm from "../components/WriteArticleForm";
+import { useState, useEffect } from "react";
 
 function WriteArticle() {
+    const [profile, setProfile] = useState(null);
+    useEffect(() => {
+        setProfile(JSON.parse(localStorage.getItem("userInfo")));
+    }, []);
+
     return (
-        <div id="feed" className="bg-gradient-to-b from-page-light-dark to-page-dark relative">
-            <AnimatedBg />
-            <div className="relative z-10">
-                <NavBar />
-                <div className="px-20 py-8">
-                    <div className="pb-4 flex flex-row justify-start items-center gap-4">
-                        <img src={feather} alt="top articles" className="h-5 w-5" />
-                        <h2 className="font-semibold text-big-title text-white">New Article</h2>
+        <>
+            {profile ? <div id="feed" className="bg-gradient-to-b from-page-light-dark to-page-dark relative">
+                < AnimatedBg />
+                <div className="relative z-10">
+                    <NavBar profile={profile} />
+                    <div className="px-20 py-8">
+                        <div className="pb-8 flex flex-row justify-start items-center gap-4">
+                            <img src={feather} alt="top articles" className="h-5 w-5" />
+                            <h2 className="font-semibold text-big-title text-white">New Article</h2>
+                        </div>
+                        <WriteArticleForm profile={profile} />
                     </div>
-                    <WriteArticleForm />
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
-        </div>
+            </div > : <div>"Loading..."</div>
+            }
+        </>
     );
 }
 
